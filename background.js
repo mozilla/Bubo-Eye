3f69096851d69d360a8606f99e03f83a74acd8b8
Yura Zenevich <yura.zenevich@gmail.com>
2018-07-24 13:14:06 -0400

    Working tesseract

--- background.js
+++ background.js
@@ -1,20 +1,20 @@
 
+// const tesseract = Tesseract.create({
+//     workerPath: "worker.js",
+//     corePath: "tesseract-core.js"
+// });
+
 browser.contextMenus.create({
   id: "discover-image-text",
   title: "Discover Image Text",// browser.i18n.getMessage("contextMenuItemDiscoverImageText"),
   contexts: ["image"],
   onclick: info => {
     debugger;
-    let canvas = document.createElement('canvas');
-    let context = canvas.getContext('2d');
-    let image = document.createElement('img');
-    image.onload = function() {
-        context.drawImage(image, 0, 0);
-        canvas.toBlob(blob => {
-          Tesseract.recognize(blob).then(result =>
-            console.log(result.text, `Confidence: ${result.confidence}%`));
-        });
-    };
-    image.src = info.srcUrl;
+    fetch(info.srcUrl)
+      .then(response => response.blob())
+      .then(blob => {
+        Tesseract.recognize(blob).then(result =>
+          console.log(result.text, `Confidence: ${result.confidence}%`));
+      });
   }
 });
