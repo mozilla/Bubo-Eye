1618ec73fd193ac30402d296594e2efff9844b8e
Yura Zenevich <yura.zenevich@gmail.com>
2018-07-24 13:25:13 -0400

    working local tesseract

--- background.js
+++ background.js
@@ -1,19 +1,18 @@
 
-// const tesseract = Tesseract.create({
-//     workerPath: "worker.js",
-//     corePath: "tesseract-core.js"
-// });
+const ocr = Tesseract.create({
+    workerPath: browser.runtime.getURL("./dist/worker.js"),
+    corePath: browser.runtime.getURL("./dist/tesseract-core.js")
+});
 
 browser.contextMenus.create({
   id: "discover-image-text",
   title: "Discover Image Text",// browser.i18n.getMessage("contextMenuItemDiscoverImageText"),
   contexts: ["image"],
   onclick: info => {
-    debugger;
     fetch(info.srcUrl)
       .then(response => response.blob())
       .then(blob => {
-        Tesseract.recognize(blob).then(result =>
+        ocr.recognize(blob).then(result =>
           console.log(result.text, `Confidence: ${result.confidence}%`));
       });
   }
