551a8dfc9835f8fcd5c4b752bee800a094ee496b
Hamlet <hamlet.villa@gmail.com>
2018-07-27 13:15:41 -0400

    README.md updated, small edits to content and manifest (#5)

--- content.js
+++ content.js
@@ -2,7 +2,7 @@ const myPort = browser.runtime.connect({name:"content-port"}); //connection to b
 
 myPort.onMessage.addListener((message) => {  // listener for message from background script
 
-  let images = document.querySelectorAll(`img[src="${message.srcUrl}"]`);  //if image repeats in page, alt attr will be changed for all.
+  let images = document.querySelectorAll(`img[src="${message.srcUrl}"]`);  //if image repeats in page, text will populate in all images.
 
   for(let image of images){
     image.setAttribute("alt", "Possible text: " + message.result.text);
