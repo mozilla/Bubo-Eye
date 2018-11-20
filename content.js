7c81c109dc73472da8e38480efd477a7542e0961
Hamlet <hamlet.villa@gmail.com>
2018-11-20 14:49:32 -0500

    added aria-describedby functionality for result confidence percentage in hidden node

--- content.js
+++ content.js
@@ -8,9 +8,16 @@ myPort.onMessage.addListener((message) => {  // Listener for message from backgr
   let startAltMsg = browser.i18n.getMessage("messageContent"); //name of message, specified in messages.json file in _locales
 
   for(let image of images){
+    let hiddenNode = document.createElement('p');
+    hiddenNode.id = "confidence";
+    hiddenNode.type = "hidden";
+
     image.setAttribute("aria-busy", "true");
     image.setAttribute("aria-live", "polite");
+    image.setAttribute("aria-describedby", "confidence");
     image.setAttribute("alt", startAltMsg + message.result.text);
+    hiddenNode.textContent = "Confidence percentage: " + message.result.confidence + "%";
+    image.appendChild(hiddenNode);
     image.setAttribute("aria-busy", "false");
   }
 
