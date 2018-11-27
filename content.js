86f660922aa64c5fe833cfdf12df09ec637144f7
Hamlet <hamlet.villa@gmail.com>
2018-11-27 12:50:41 -0500

    edits in messages.json; deleted comment in background.js; optimized loop in content.js

--- content.js
+++ content.js
@@ -5,10 +5,9 @@ myPort.onMessage.addListener((message) => {  // Listener for message from backgr
   // If image repeats in page, text will populate in all images.
   let images = document.querySelectorAll(`img[src="${message.srcUrl}"]`);
 
-  let startAltMsg = browser.i18n.getMessage("messageContent"); //name of message, specified in messages.json file in _locales
-
   for(let image of images){
-    image.setAttribute("alt", startAltMsg + message.result.text);
+    image.setAttribute("alt", browser.i18n.getMessage("messageContent",
+    message.result.text));
   }
 
 });
