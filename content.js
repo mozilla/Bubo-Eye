ff4b70c0473acc6c24f1755c862b54fd5bb5eefc
Hamlet <hamlet.villa@gmail.com>
2018-12-04 15:54:36 -0500

    remove unneccesary variable

--- content.js
+++ content.js
@@ -5,7 +5,6 @@ myPort.onMessage.addListener((message) => {  // Listener for message from backgr
   // If image repeats in page, text will populate in all images.
   let images = document.querySelectorAll(`img[src="${message.srcUrl}"]`);
 
-  //let startAltMsg = browser.i18n.getMessage("messageContent"); //name of message, specified in messages.json file in _locales
   function resultFunction(){
     let resultElement = document.createElement("p");
     resultElement.id = "a11yElem";
