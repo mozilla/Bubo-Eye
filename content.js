151ce7ddac0ab34fe2b56e28f1d53f08ff8a28be
Hamlet <hamlet.villa@gmail.com>
2018-12-04 15:52:42 -0500

    results of scanning pushed to hidden node with aria-describedby instead of using alt attribute

--- content.js
+++ content.js
@@ -5,11 +5,19 @@ myPort.onMessage.addListener((message) => {  // Listener for message from backgr
   // If image repeats in page, text will populate in all images.
   let images = document.querySelectorAll(`img[src="${message.srcUrl}"]`);
 
-  let startAltMsg = browser.i18n.getMessage("messageContent"); //name of message, specified in messages.json file in _locales
+  //let startAltMsg = browser.i18n.getMessage("messageContent"); //name of message, specified in messages.json file in _locales
+  function resultFunction(){
+    let resultElement = document.createElement("p");
+    resultElement.id = "a11yElem";
+    resultElement.style.display = "none";
+    document.body.appendChild(resultElement);
+    resultElement.textContent = browser.i18n.getMessage("messageContent") + message.result.text;
+    resultElement.setAttribute("aria-live", "polite");
+  }
 
   for(let image of images){
-    image.setAttribute("aria-live", "polite");
-    image.setAttribute("alt", startAltMsg + message.result.text);
+    image.setAttribute("aria-describedby", "a11yElem");
+    resultFunction();
   }
 
 });
