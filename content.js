95e4273903a7a03f68ab53a0d7f3205dece729f1
Yura Zenevich <yura.zenevich@gmail.com>
2018-12-05 15:14:09 -0500

    Revert "Merge pull request #31 from hamletv/14"
    This reverts commit f29c09c52b76f7eb9d663b7e3f5f0965c51ca1e6, reversing
    changes made to 2fe4dfed3464d36859c603922ed1ba5a3b378f74.

--- content.js
+++ content.js
@@ -5,15 +5,6 @@ myPort.onMessage.addListener((message) => {  // Listener for message from backgr
   // If image repeats in page, text will populate in all images.
   let images = document.querySelectorAll(`img[src="${message.srcUrl}"]`);
 
-  function confidenceHiddenNode(){
-    let confidenceNode = document.createElement('p');
-    confidenceNode.id = "confidence";
-    confidenceNode.type = "hidden";
-
-    confidenceNode.textContent = browser.i18n.getMessage("confidenceLocalized") + message.result.confidence + "%";
-    document.body.appendChild(confidenceNode);
-  }
-
   for(let image of images){
     image.setAttribute("alt", browser.i18n.getMessage("messageContent",
       message.result.text));
