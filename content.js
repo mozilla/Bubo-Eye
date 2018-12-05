90a3b18e2b5e392306857416179a4244a3edde79
Yura Zenevich <yura.zenevich@gmail.com>
2018-12-05 15:09:18 -0500

    Revert "Merge pull request #29 from hamletv/12"
    This reverts commit b605b93abd40230e9a93bde83dafee15979c5805, reversing
    changes made to f29c09c52b76f7eb9d663b7e3f5f0965c51ca1e6.

--- content.js
+++ content.js
@@ -5,27 +5,18 @@ myPort.onMessage.addListener((message) => {  // Listener for message from backgr
   // If image repeats in page, text will populate in all images.
   let images = document.querySelectorAll(`img[src="${message.srcUrl}"]`);
 
-  function resultFunction(){
-    let resultElement = document.createElement("p");
-    resultElement.id = "a11yElem";
-    resultElement.style.display = "none";
-    document.body.appendChild(resultElement);
-    resultElement.textContent = browser.i18n.getMessage("messageContent") + message.result.text;
-    resultElement.setAttribute("aria-live", "polite");
-  }
-
   function confidenceHiddenNode(){
     let confidenceNode = document.createElement('p');
     confidenceNode.id = "confidence";
     confidenceNode.type = "hidden";
+
     confidenceNode.textContent = browser.i18n.getMessage("confidenceLocalized") + message.result.confidence + "%";
     document.body.appendChild(confidenceNode);
   }
 
   for(let image of images){
-    image.setAttribute("aria-describedby", "a11yElem");
-    resultFunction();
-    confidenceHiddenNode();
+    image.setAttribute("alt", browser.i18n.getMessage("messageContent",
+      message.result.text));
   }
 
 });
